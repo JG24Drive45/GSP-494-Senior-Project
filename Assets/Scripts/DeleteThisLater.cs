53dca14dec3b61ee4f561e922eaed03e4d8648c0
Jill <jg24drive45@gmail.com>
2014-07-17 18:53:04 -0400

    Concept Art
    Added some images for possible ships to use

--- Assets/Scripts/DeleteThisLater.cs
+++ Assets/Scripts/DeleteThisLater.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DeleteThisLater : MonoBehaviour {
+public class DeleteThisLater : MonoBehaviour 
+{
+	public float hSpeed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +12,10 @@ public class DeleteThisLater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Update rotation
+		transform.rotation = Quaternion.Euler( new Vector3( 0.0f, -Input.GetAxis( "Horizontal" ) * 20.0f, 0.0f ) );
+
+		// Update position
+		transform.position += new Vector3( Input.GetAxis( "Horizontal" ) * hSpeed * Time.deltaTime, 0.0f, 0.0f );
 	}
 }
