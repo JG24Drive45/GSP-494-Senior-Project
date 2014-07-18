0e0866fac41dbb09e2bd31b3cf440105727f883e
Jill <jg24drive45@gmail.com>
2014-07-18 14:50:29 -0400

    Added more potential art assets

--- Assets/Scripts/DeleteThisLater.cs
+++ Assets/Scripts/DeleteThisLater.cs
@@ -13,7 +13,7 @@ public class DeleteThisLater : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 		// Update rotation
-		transform.rotation = Quaternion.Euler( new Vector3( 0.0f, -Input.GetAxis( "Horizontal" ) * 20.0f, 0.0f ) );
+		transform.rotation = Quaternion.Euler( new Vector3( 0.0f, -Input.GetAxis( "Horizontal" ) * 20.0f, 180.0f ) );
 
 		// Update position
 		transform.position += new Vector3( Input.GetAxis( "Horizontal" ) * hSpeed * Time.deltaTime, 0.0f, 0.0f );
