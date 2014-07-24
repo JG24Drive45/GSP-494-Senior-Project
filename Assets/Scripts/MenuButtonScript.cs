beabd721ec584e0946993aa49b179c0c639b22d8
Jill <jg24drive45@gmail.com>
2014-07-24 17:48:33 -0400

    Simple Generic Menu Button

---
+++ Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonScript : MonoBehaviour 
+{
+	// Button Attributes
+	public float fButtonWidth;
+	public float fButtonHeight;
+	public string sButtonText;
+
+	public float fXPositionRate;					// Range from 0.0f to 1.0f
+	public float fYPositionRate;					// Range from 0.0f to 1.0f
+
+	public float fFontSizeRate;						// Range from 0.0f to 1.0f
+
+	public GUIStyle guiStyle;
+
+
+	void Awake()
+	{
+		guiStyle.fontSize = (int)( Screen.height * fFontSizeRate );
+	}
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if( GUI.Button( new Rect ( ( Screen.width * fXPositionRate ) - ( fButtonWidth / 2.0f ), ( Screen.height * fYPositionRate ) - ( fButtonHeight / 2.0f ), 
+		                          fButtonWidth, fButtonHeight ), sButtonText, guiStyle ) )
+		{
+			Debug.Log( "Clicked Button" );
+		}
+	}
+}
