36999ef6e777f023e934131f5a088c20f18e76d7
Jill <jg24drive45@gmail.com>
2014-07-25 06:09:56 -0400

    Start of HUD

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour 
+{
+	public float health = 1.0f;					// Testing
+	public float shields = 1.0f;				// Testing
+
+	void Awake()
+	{
+
+	}
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Keypad1))						// Testing
+		{
+			if( health > 0.0f )
+			{
+				health -= 0.01f;
+				if( health < 0.0f )
+					health = 0.0f;
+				UpdateHealthBar();
+			}
+		}
+		else if (Input.GetKey(KeyCode.Keypad3))				// Testing
+		{
+			if( shields > 0.0f )
+			{
+				shields -= 0.01f;
+				if( shields < 0.0f )
+					shields = 0.0f;
+				UpdateHealthBar();
+			}
+		}
+		else if( Input.GetKey(KeyCode.Keypad4))
+		{
+			if( health < 1.0f )
+			{
+				health += 0.01f;
+				if( health > 1.0f )
+					health = 1.0f;
+				UpdateHealthBar();
+			}
+		}
+		else if( Input.GetKey(KeyCode.Keypad6))
+		{
+			if( shields < 1.0f )
+			{
+				shields += 0.01f;
+				if( shields > 1.0f )
+					shields = 1.0f;
+				UpdateHealthBar();
+			}
+		}
+	}
+
+	public void UpdateHealthBar()
+	{
+		Renderer temp = GameObject.Find( "LeftHealthBar" ).GetComponent<Renderer>();
+		temp.material.SetFloat( "_HealthPercentage", -( 1.0f - health ) );
+		temp.material.SetFloat( "_Inverter", -1.0f );
+
+
+		GameObject.Find( "RightHealthBar" ).GetComponent<Renderer>().material.SetFloat( "_HealthPercentage", shields );
+	}
+}
---
+++ Assets/Shaders/HealthBarShader.shader
@@ -0,0 +1,49 @@
+﻿Shader "Shaders/Healthbar Shader" 
+{
+	Properties 
+	{
+    	_MainTex ("Base (RGB) Trans (A)", 2D) = "white" {}
+    	_HealthPercentage("Health Percentage", float ) = 1.0
+    	_Inverter("Inverter", float ) = 1.0
+	}
+ 
+	SubShader 
+	{
+   		Tags {"Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+    	LOD 200
+ 
+		CGPROGRAM
+		#pragma surface surf Lambert alpha
+ 
+		uniform sampler2D _MainTex;
+		uniform float _HealthPercentage;
+		uniform float _Inverter;
+ 
+		struct Input 
+		{
+    		float2 uv_MainTex;
+		};
+ 
+		void surf (Input IN, inout SurfaceOutput o) 
+		{
+    		fixed4 col = tex2D(_MainTex, IN.uv_MainTex);
+    		
+    		// Draw what health the player has
+    		if( col.a != 0 && ( _Inverter * IN.uv_MainTex.x ) <= _HealthPercentage )
+    		{
+    			o.Albedo = col.rgb;
+    			o.Alpha = col.a;
+    		}
+    		// Draw what health the player has lost
+    		else if( col.a != 0 && IN.uv_MainTex.x > _HealthPercentage )
+    		{
+    			col = float4( 1.0f, 0.0f, 0.0f, col.a );
+    			o.Albedo = col.rgb;
+    			o.Alpha = col.a;
+    		}
+		}
+		ENDCG
+	}
+ 
+	Fallback "Transparent/Diffuse"
+}
