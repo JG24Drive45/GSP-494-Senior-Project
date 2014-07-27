85f58c746e86527b7554ad164e31f2b77ec1dd70
EricH <skyfjet2@yahoo.com>
2014-07-27 01:31:56 -0400

    Sample Dynamic Narration
    Scripting set up to hold speaker's name and words in boxes.
    Can click through the dialogue until it reaches the end.

---
+++ Assets/Scripts/DisplayNarration.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class NarrativePiece
+{
+	public string speaker;
+	public string narration;
+	public NarrativePiece(string pSpeaker, string pNarration)
+	{
+		this.speaker = pSpeaker;
+		this.narration = pNarration;
+	}
+};
+
+class SceneNarration
+{
+	public List<NarrativePiece> narration;
+	public SceneNarration()
+	{
+		this.narration = new List<NarrativePiece> ();
+	}
+};
+
+public class DisplayNarration : MonoBehaviour
+{
+
+	public Rect speakerBox;
+	public Rect narrationBox;
+
+	public GUIStyle boxStyle;
+
+	public int sceneID;
+	private int narrativeIndex = 0;
+
+	private List<SceneNarration> gameNarrative = new List<SceneNarration>();
+	private SceneNarration scene1 = new SceneNarration();
+	private SceneNarration scene2 = new SceneNarration();
+	// Use this for initialization
+	void Start ()
+	{
+		scene1.narration.Add (new NarrativePiece("Staff Sergeant Jennifer Greenheart",
+		                                        "Get to work, welp!")
+		        );
+		scene1.narration.Add (new NarrativePiece ("Private Dakota Grey",
+		                                         "Yes, ma'am!")
+		        );
+		gameNarrative.Add (scene1);
+		scene2.narration.Add (new NarrativePiece ("Staff Sergeant Dakota Grey",
+		                                        "Wait.. I'm in charge now..? MUAHAHAHAHA. LICK MY BOOT, PRIVATE!")
+				);
+		scene2.narration.Add (new NarrativePiece ("Private Jennifer Green",
+		                                        ";_;")
+				);
+		scene2.narration.Add (new NarrativePiece ("Narrator",
+		                                         "Get your fanfic pens out..")
+				);
+		gameNarrative.Add (scene2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (speakerBox, gameNarrative[sceneID].narration[narrativeIndex].speaker, boxStyle);
+		//GUI.Label (narrationBox, sceneNarrative[narrativeIndex].narration, boxStyle);
+		if (GUI.Button (narrationBox, gameNarrative[sceneID].narration[narrativeIndex].narration, boxStyle))
+		{
+			if (narrativeIndex+1 < gameNarrative[sceneID].narration.Count)
+				narrativeIndex++;
+		}
+	}
+}
