8ebd50bc2df84ebb07abd274f253882a07cfedd1
Jill <jg24drive45@gmail.com>
2014-08-03 01:40:03 -0400

    HUD Update
    Added more placeholders to the HUD game object

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -3,20 +3,45 @@ using System.Collections;
 
 public class HUDScript : MonoBehaviour 
 {
-	public float health = 1.0f;					// Testing
-	public float shields = 1.0f;				// Testing
+	// Public
+	public float health 	= 1.0f;							// Testing
+	public float shields 	= 1.0f;							// Testing
 
+	public int score		= 0;							// Testing
+	public int debris		= 0;							// Testing
+
+	public GUIStyle guiStyle;
+
+	// Private	
+	private Rect scoreRect;
+	private Rect debrisRect;
+
+	private float fLabelWidthRate	= 0.165f;
+	private float fLabelHeightRate	= 0.075f;
+
+	private float fFontSizeRate		= 0.05f;
+
+	#region void Awake()
 	void Awake()
 	{
+		int iScreenWidth	 = Screen.width;
+		int iScreenHeight	 = Screen.height;
+		int labelWidth, labelHeight;
 
-	}
+		//Calculate the label size based on screen size
+		labelWidth	= (int)( fLabelWidthRate * Screen.width );
+		labelHeight	= (int)( fLabelHeightRate * Screen.height );
 
-	void Start () 
-	{
-	
+		// Calculate the Rects based on screen size
+		scoreRect = new Rect( 0.1f * iScreenWidth, 0.03f * iScreenHeight, labelWidth, labelHeight );
+		debrisRect = new Rect ( iScreenWidth * 0.9f - labelWidth, 0.03f * iScreenHeight, labelWidth, labelHeight );
+
+		// Calculate the font based on screen size
+		guiStyle.fontSize = (int)( fFontSizeRate * iScreenHeight );
 	}
-	
-	// Update is called once per frame
+	#endregion
+
+	#region void Update()
 	void Update () 
 	{
 		if(Input.GetKey(KeyCode.Keypad1))						// Testing
@@ -59,15 +84,49 @@ public class HUDScript : MonoBehaviour
 				UpdateHealthBar();
 			}
 		}
-	}
 
+		if( Input.GetKeyDown( KeyCode.Return ) )
+		{
+			UpdateScore( 10 );
+		}
+
+		if( Input.GetKeyDown( KeyCode.Backspace ) )
+		{
+			UpdateDebris( 50 );
+		}
+	}
+	#endregion
+
+	#region public void UpdateHealthBar()
 	public void UpdateHealthBar()
 	{
 		Renderer temp = GameObject.Find( "LeftHealthBar" ).GetComponent<Renderer>();
 		temp.material.SetFloat( "_HealthPercentage", -( 1.0f - health ) );
 		temp.material.SetFloat( "_Inverter", -1.0f );
 
-
 		GameObject.Find( "RightHealthBar" ).GetComponent<Renderer>().material.SetFloat( "_HealthPercentage", shields );
 	}
+	#endregion
+
+	#region public void UpdateScore( int points )
+	public void UpdateScore( int points )
+	{
+		score += points;									// Update the variable
+	}
+	#endregion
+
+	#region public void UpdateDebris()
+	public void UpdateDebris( int scrap )
+	{
+		debris += scrap;
+	}
+	#endregion
+
+	#region void OnGUI()
+	void OnGUI()
+	{
+		GUI.Label( scoreRect, score.ToString(), guiStyle );			// Display the score
+		GUI.Label( debrisRect, debris.ToString(), guiStyle );			// Display the debris count
+	}
+	#endregion
 }
