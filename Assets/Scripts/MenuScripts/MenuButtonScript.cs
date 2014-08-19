1a87e0d70bfaf60c86696f9b6816bf1c4a03965d
Jill <jg24drive45@gmail.com>
2014-08-19 00:01:01 -0400

    Menu Button Script Update

--- Assets/Scripts/MenuScripts/MenuButtonScript.cs
+++ Assets/Scripts/MenuScripts/MenuButtonScript.cs
@@ -1,97 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class MenuButtonScript : MonoBehaviour 
 {
 	public Material[] materials;
-	public Material[] onHoverMaterials;
 
-	private RaycastHit 	hit;
-	private Ray			ray;
-
-	public List<Renderer> buttonRenderers = new List<Renderer>();
-
-	#region void Awake()
-	void Awake()
+	#region void OnMouseEnter()
+	void OnMouseEnter()
 	{
-		buttonRenderers.Add( GameObject.Find( "PlayGameButton" ).GetComponent<Renderer>() );
-		buttonRenderers.Add( GameObject.Find( "LoadGameButton" ).GetComponent<Renderer>() );
-		buttonRenderers.Add( GameObject.Find( "InstructionsButton" ).GetComponent<Renderer>() );
-		buttonRenderers.Add( GameObject.Find( "OptionsButton" ).GetComponent<Renderer>() );
-		buttonRenderers.Add( GameObject.Find( "CreditsButton" ).GetComponent<Renderer>() );
-		buttonRenderers.Add( GameObject.Find( "ExitButton" ).GetComponent<Renderer>() );
+		this.gameObject.renderer.material = materials[1];
 	}
 	#endregion
 
-	#region void Update()
-	void Update () 
+	#region void OnMouseExit()
+	void OnMouseExit()
 	{
-		ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		this.gameObject.renderer.material = materials[0];
+	}
+	#endregion
 
-		// Change Button material if it is hovered over
-		if( Physics.Raycast( ray, out hit ) )
+	#region void OnMouseUp()
+	void OnMouseUp()
+	{
+		switch( this.gameObject.name )
 		{
-			switch( hit.collider.name )
-			{
-			case "PlayGameButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[0];
-				break;
-			case "LoadGameButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[1];
-				break;
-			case "InstructionsButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[2];
-				break;
-			case "OptionsButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[3];
-				break;
-			case "CreditsButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[4];
-				break;
-			case "ExitButton":
-				hit.collider.GetComponent<Renderer>().material = onHoverMaterials[5];
-				break;
-			case "ControlPanel":
-				for( int i = 0; i < buttonRenderers.Count; i++ )
-				{
-					buttonRenderers[i].material = materials[i];
-				}
-				break;
-
-			}
-		}
-
-		// Check for Button Clicks
-		if( Input.GetMouseButtonUp(0) )
-		{
-			if( Physics.Raycast( ray, out hit ) )
-			{
-				switch( hit.collider.name )
-				{
-				case "PlayGameButton":
-					Debug.Log( "Clicked Play Game Button" );
-					break;
-				case "LoadGameButton":
-					Debug.Log( "Clicked Load Game Button" );
-					break;
-				case "InstructionsButton":
-					Debug.Log( "Clicked Instructions Button" );
-					Application.LoadLevel( "Instructions" );
-					break;
-				case "OptionsButton":
-					Debug.Log( "Clicked Options Button" );
-					Application.LoadLevel( "Options" );
-					break;
-				case "CreditsButton":
-					Debug.Log( "Clicked Credits Button" );
-					Application.LoadLevel( "Credits" );
-					break;
-				case "ExitButton":
-					Debug.Log( "Clicked Exit Button" );
-					break;
-				}
-			}
+		case "PlayGameButton":
+			Debug.Log( "Clicked the play game button" );
+			break;
+		case "LoadGameButton":
+			Debug.Log( "Clicked the load game button" );
+			break;
+		case "InstructionsButton":
+			Debug.Log( "Clicked the instructions button" );
+			Application.LoadLevel( "Instructions" );
+			break;
+		case "OptionsButton":
+			Debug.Log( "Clicked the options button" );
+			Application.LoadLevel( "Options" );
+			break;
+		case "CreditsButton":
+			Debug.Log( "Clicked the credits button" );
+			Application.LoadLevel( "Credits" );
+			break;
+		case "ExitButton":
+			Debug.Log( "Clicked the exit button" );
+			break;
+		case "BackButton":
+			Debug.Log( "Clicked the back button" );
+			Application.LoadLevel( "MainMenu" );
+			break;
 		}
 	}
 	#endregion
