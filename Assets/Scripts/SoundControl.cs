3f3e4c94f6412c1c22aa503d51da4f27dfe3dc4e
EricH <skyfjet2@yahoo.com>
2014-08-20 00:21:53 -0400

    Start of sound management
    Testing a sound manager

---
+++ Assets/Scripts/SoundControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour {
+
+	public void PlaySound(string p_pathFileName)
+	{
+		audio.clip = (AudioClip)Resources.Load ("Sounds/" + p_pathFileName);
+		audio.PlayOneShot (audio.clip);
+	}
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (GameObject.Find("SoundManager"));
+		PlaySound ("Menu/ButtonHover");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
