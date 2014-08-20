ed06f396e0aaab336820398968232360c552e11c
Jill <jg24drive45@gmail.com>
2014-08-20 00:46:15 -0400

    Sound Update

--- Assets/Scripts/MenuScripts/MenuButtonScript.cs
+++ Assets/Scripts/MenuScripts/MenuButtonScript.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class MenuButtonScript : MonoBehaviour 
 {
+	public delegate void SFX();
+
+	public static event SFX onMouseOver;
+	public static event SFX onMouseClick;
+
 	public Material[] materials;
 
 	#region void OnMouseEnter()
 	void OnMouseEnter()
 	{
+		if( onMouseOver != null )
+			onMouseOver();
 		this.gameObject.renderer.material = materials[1];
 	}
 	#endregion
@@ -25,27 +32,41 @@ public class MenuButtonScript : MonoBehaviour
 		switch( this.gameObject.name )
 		{
 		case "PlayGameButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the play game button" );
 			break;
 		case "LoadGameButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the load game button" );
 			break;
 		case "InstructionsButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the instructions button" );
 			Application.LoadLevel( "Instructions" );
 			break;
 		case "OptionsButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the options button" );
 			Application.LoadLevel( "Options" );
 			break;
 		case "CreditsButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the credits button" );
 			Application.LoadLevel( "Credits" );
 			break;
 		case "ExitButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the exit button" );
 			break;
 		case "BackButton":
+			if( onMouseClick != null )
+				onMouseClick();
 			Debug.Log( "Clicked the back button" );
 			Application.LoadLevel( "MainMenu" );
 			break;
--- Assets/Scripts/SoundControl.cs
+++ Assets/Scripts/SoundControl.cs
@@ -1,22 +1,77 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class SoundControl : MonoBehaviour {
+//
+//	public void PlaySound(string p_pathFileName)
+//	{
+//		audio.clip = (AudioClip)Resources.Load ("Sounds/" + p_pathFileName);
+//		audio.PlayOneShot (audio.clip);
+//	}
+//
+//	// Use this for initialization
+//	void Start () {
+//		DontDestroyOnLoad (GameObject.Find("SoundManager"));
+//		PlaySound ("Menu/ButtonHover");
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//}
+
+
+using UnityEngine;
 using System.Collections;
 
-public class SoundControl : MonoBehaviour {
-
-	public void PlaySound(string p_pathFileName)
+public class SoundControl : MonoBehaviour 
+{
+	private static SoundControl instance = null;
+	
+	public AudioClip onHover;
+	public AudioClip onClick;
+	
+	public static SoundControl GetInstance
 	{
-		audio.clip = (AudioClip)Resources.Load ("Sounds/" + p_pathFileName);
-		audio.PlayOneShot (audio.clip);
+		get
+		{
+			return instance;
+		}
+	}
+	
+	void Awake()
+	{
+		if( instance != null && instance != this )
+		{
+			Destroy( this.gameObject );
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		
+		DontDestroyOnLoad( this.gameObject );
+
+		// Subscribe to events
+		MenuButtonScript.onMouseOver += PlayOnHover;
+		MenuButtonScript.onMouseClick += PlayOnClick;
 	}
 
-	// Use this for initialization
-	void Start () {
-		DontDestroyOnLoad (GameObject.Find("SoundManager"));
-		PlaySound ("Menu/ButtonHover");
+	void OnDestroy()
+	{
+		MenuButtonScript.onMouseOver -= PlayOnHover;
+		MenuButtonScript.onMouseClick -= PlayOnClick;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void PlayOnHover()
+	{
+		audio.PlayOneShot( onHover );
+	}
+
+	void PlayOnClick()
+	{
+		audio.PlayOneShot( onClick );
 	}
 }
