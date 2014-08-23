0f66f4a02dac0c1852375380acd517ab3f315ce9
Jordan Huffman <jordan.huffman@hotmail.com>
2014-08-22 19:20:17 -0500

    Enemy 1 AI v1

--- Assets/Scripts/Enemy1AI.cs
+++ Assets/Scripts/Enemy1AI.cs
@@ -5,13 +5,23 @@ public class Enemy1AI : MonoBehaviour
 {
 	public float speed;					// how fast the ship moves
 	public int health;					// health of AI
+	public bool moving;					// if the ship is moving
 	public Vector3 destination;			// destination of ship
 	public Vector3 distance;			// distance to the destination
+	public Transform target;			// the target of the AI destination
 
 	public Camera myCamera;				// main camera
+	public int xMin;
+	public int xMax;
+
 	void Start () 
 	{
-		speed = 1.5f;
+		target = GameObject.Find("Player").transform;
+		speed = 5.0f;
+		myCamera = Camera.main;
+		xMin = 25;
+		xMax = Screen.width - 25;
+		SetDestination();
 	}
 
 	void Update () 
@@ -21,7 +31,35 @@ public class Enemy1AI : MonoBehaviour
 
 	void Movement()
 	{
+		if(!moving)
+		{
+			distance = destination - transform.position;								// get the distance left till target is reached
+			distance.Normalize(); 														// normalize the distance vector to get vector with direction and magnitude
+			moving = true;
+		}
+		if(moving)
+		{
+			transform.Translate(distance * speed * Time.deltaTime, Space.World);
+		}
 
+		if( transform.position.x <= -8.0f || transform.position.x >= 8.0f || transform.position.y >= 6.0f || transform.position.y <= -6.0f )
+		{
+			RepositionEnemey();
+		}
+
+	}
+
+	void SetDestination()
+	{
+		destination = new Vector3(target.position.x, target.position.y + 1, 0.0f);		// set destination to a little bit past the intended target
+	}
+
+	void RepositionEnemey()
+	{
+		transform.position = new Vector3( myCamera.ScreenToWorldPoint( new Vector3( Random.Range( xMin, xMax ), 0f, 0f ) ).x, 
+		                             myCamera.ScreenToWorldPoint( new Vector3 (0f, Screen.height, 0f ) ).y,
+		                                 -1.0f);
+		moving = false;		// reset moving to false to calculate new destination
 	}
 
 }
