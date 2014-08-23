31cb1151c509fa0ed90fd8d5779c2517b9410e6e
EricH <skyfjet2@yahoo.com>
2014-08-22 21:04:22 -0400

    Working on the opening scene
    Added comments to the code for legibility
    Added name and profile image variables for easier updating in the future
    Added a majority of scene 1 (no introductions yet)

--- Assets/Scripts/DisplayNarration.cs
+++ Assets/Scripts/DisplayNarration.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//Simple data storage for a piece of narrative - profile image, speaker name, speaker dialogue
 class NarrativePiece
 {
 	public Texture image;
@@ -15,6 +16,7 @@ class NarrativePiece
 	}
 };
 
+//Simple data storage for a scene - list of NarrativePieces
 class SceneNarration
 {
 	public List<NarrativePiece> narration;
@@ -26,12 +28,12 @@ class SceneNarration
 
 public class DisplayNarration : MonoBehaviour
 {
-
+	//Loads an image as needed (may be substituted for public Textures later)
 	private Texture GrabImg(string pFileName)
 	{
 		return (Texture)Resources.Load("Art/Character Profile Images/" + pFileName);
 	}
-
+	//Adds a piece of narrative to a scene
 	private void AddNarr(SceneNarration pScene, string pImg, string pSpeaker, string pNarration)
 	{
 		pScene.narration.Add (new NarrativePiece (GrabImg (pImg),
@@ -40,18 +42,49 @@ public class DisplayNarration : MonoBehaviour
 		                      );
 	}
 
+	//Location of speaker's name
 	private Rect speakerBox;
+	//Location of dialogue box
 	private Rect narrationBox;
+	//Location of the character's profile image
 	private Rect imageBox;
 
+	//Style of the box
 	public GUIStyle boxStyle;
 
+	//Current scene to display
 	public int sceneID;
+	//Current line of dialogue to display
 	private int narrativeIndex = 0;
 
+	//Profile image name shortcuts
+	private string imgBlank = "";
+	private string imgDakota = "";
+	private string imgWarringer = "";
+	private string imgJoshHarbor = "";
+	private string imgIsabellaIvanova = "";
+	private string imgHansBlau = "";
+	private string imgHirokoTai = "";
+
+	//Character name shortcuts
+	private string nameMale = "Male ";
+	private string nameFemale = "Female ";
+	private string nameNone = "";
+	private string nameNarrator = "Narrator";
+	private string nameNewsRep = "News Reporter";
+	private string nameMilRep = "Military Representative";
+	private string nameSergWarringer = "Sergeant Carson Warringer";
+	private string namePrivDakota = "Private Dakota Grey";
+	private string nameSpecJoshua = "Specialist Joshua Harbor";
+	private string namePrivIsabella = "Private Isabella Ivanova";
+	private string namePrivHans = "Private Hans Blau";
+	private string nameSpecHiroko = "Specialist Hiroko Tai";
+
+	//Full list of the game's narrative
 	private List<SceneNarration> gameNarrative = new List<SceneNarration>();
+	//Individual scenes
 	private SceneNarration scene1 = new SceneNarration();
-	private SceneNarration scene2 = new SceneNarration();
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -69,28 +102,76 @@ public class DisplayNarration : MonoBehaviour
 		imageBox.yMin = imageBox.yMax - (Screen.height / 2);
 
 		AddNarr (scene1,
-		         "Craine Sprite",
-		         "Staff Sergeant Jennifer Greenheart",
-		         "Get to work, welp!");
+		         imgBlank,
+		         nameFemale + nameNewsRep,
+		         "The Federation of Humanity claim to have found a " +
+		         "source of ‘X01’, the ingredient necessary for creating the " +
+		         "antidote to Gymna, the deadly disease that has spread " +
+		         "throughout the planets of humanity");
 		AddNarr (scene1,
-		         "Craine Sprite",
-				 "Private Dakota Grey",
-				 "Yes, ma'am!");
+		         imgBlank,
+		         nameFemale + nameNewsRep,
+		         "Unfortunately, the source is already being utilized " +
+		         "by an alien race with a population fifty times larger " +
+		         "than humanity’s.");
+		AddNarr (scene1,
+		         imgBlank,
+		         nameFemale + nameNewsRep,
+		         "The FoH attempted peaceful negotiations with the alien " +
+		         "race, but they fell through. The FoH declared war shortly " +
+		         "after, claiming that it was the only way to save the human " +
+		         "race from this terrible disease and the greed of these aliens.");
+		AddNarr (scene1,
+		         imgBlank,
+		         nameFemale + nameNewsRep,
+		         "We have with us a representative from the military" +
+				 "to explain what the FoH's goals are with this war.");
+		AddNarr (scene1,
+		         imgBlank,
+		         nameMale + nameMilRep,
+		         "The alien race actually uses the mineral to construct " +
+		         "their ships and other mechanical structures, so the primary " +
+		         "goal is the neutralization and scrapping of these structures.");
+		AddNarr (scene1,
+		         imgBlank,
+		         nameMale + nameMilRep,
+		         "The FoH is still looking for new recruits to help save" +
+				 "humanity by fighting against the large armadas of our foes.");
+		AddNarr (scene1,
+		         imgDakota,
+		         namePrivDakota,
+		         "I decided to join the war effort shortly after hearing that " +
+		         "report. It wasn’t a difficult decision, seeing as most of " +
+		         "my family had already been infected with Gymna. I had a " +
+		         "letter sent to them, since they couldn’t be let out of " +
+		         "quarantine. I’m sure they understand. There’s not much " +
+		         "point in going on if everybody I know is dead by the end " +
+		         "of the year. Besides… the FoH says this war can save them. " +
+		         "I need to do my part… Ah. Someone’s coming.");
+		AddNarr (scene1,
+		         imgWarringer,
+		         nameNone,
+		         "A pleasant-looking man stands in front of you and your " +
+		         "squad with his arms folded. He begins to speak with a " +
+		         "warm voice.");
+		AddNarr (scene1,
+		         imgWarringer,
+		         nameSergWarringer,
+		         "Welcome, privates. I am Sergeant Carson Warringer. You may " +
+		         "address me as ‘Sergeant’ or ‘Sergeant Warringer’. I will " +
+		         "be giving you all your orders from here on.");
+		//Introductions
+		AddNarr (scene1,
+		         imgWarringer,
+		         nameSergWarringer,
+		         "Good. Now that you all know why your allies are fighting, " +
+		         "I hope that you can respect those reasons. You need to " +
+		         "respect and trust each other to become respected and " +
+		         "trustworthy soldiers. That is all for now. You have been " +
+		         "given a tour of the facility. You will go through basic " +
+		         "training one last time tomorrow at 0800 hours. You are " +
+		         "free until then. Do not be late. Dismissed.");
 		gameNarrative.Add (scene1);
-
-		AddNarr (scene2,
-		         "Craine Sprite",
-		         "Staff Sergeant Dakota Grey",
-		         "Wait.. I'm in charge now..? MUAHAHAHAHA. LICK MY BOOT, PRIVATE!");
-		AddNarr (scene2,
-		         "Craine Sprite",
-		         "Private Jennifer Green",
-		         ";_;");
-		AddNarr (scene2,
-		         "Craine Sprite",
-		         "Narrator",
-		         "Get your fanfic pens out..");
-		gameNarrative.Add (scene2);
 	}
 	
 	// Update is called once per frame
