abc8e532219b3d2ad98127df506b927eaf3c1023
Jill <jg24drive45@gmail.com>
2014-09-27 16:17:10 -0400

    Main Menu Enemies and scrolling background
    Created enemies that move in the main menu and set up a scrolling
    background for stars

---
+++ Assets/Scripts/MenuScripts/MenuEnemyScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEnemyScript : MonoBehaviour 
+{
+	public float speed = 0.05f;
+	public float dir;
+
+	void Start()
+	{
+		switch( gameObject.name )
+		{
+		case "MenuEnemy1":
+			SetRandomX();
+			break;
+		case "MenuEnemy2":
+			SetRandomX();
+			break;
+		case "MenuEnemy3":
+			SetRandomX();
+			break;
+		}
+	}
+
+	void Update()
+	{
+		switch( gameObject.name )
+		{
+		case "MenuEnemy1":
+			transform.position += new Vector3( 0.0f, -speed, 0.0f );
+			if( transform.position.y <= -5.5f )
+				SetRandomX();
+			break;
+		case "MenuEnemy2":
+			transform.position += new Vector3( dir, -speed, 0.0f );
+			if( transform.position.y <= -5.5f || transform.position.x <= -5.5f || transform.position.x >= 5.5f )
+				SetRandomX();
+			break;
+		case "MenuEnemy3":
+			transform.position += new Vector3( dir, -speed, 0.0f );
+			if( transform.position.x <= -5.5f || transform.position.x >= 5.5f )
+				dir *= -1;
+			if( transform.position.y <= -5.5f )
+				SetRandomX();
+			break;
+		}
+	}
+
+	void SetRandomX()
+	{
+		float x = Random.Range( -5.5f, 5.5f );
+
+		switch( gameObject.name )
+		{
+		case "MenuEnemy1":
+			transform.position = new Vector3 ( x, 5.5f, 0.0f );
+			break;
+		case "MenuEnemy2":
+			dir = Random.Range( 0, 2 );
+			if( dir == 0 )
+				dir = -0.03f;
+			else
+				dir = 0.03f;
+			transform.position = new Vector3( x, 5.5f, 0.0f );
+			break;
+		case "MenuEnemy3":
+			transform.position = new Vector3( x, 5.5f, 0.0f );
+			dir = Random.Range( 0, 2);
+			if( dir == 0 )
+				dir = 0.05f;
+			else
+				dir = -0.05f;
+			speed = 0.03f;
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/MenuScripts/ScrollingBackgroundScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackgroundScript : MonoBehaviour 
+{
+	public float scrollSpeed = 0.001f;
+	public float moveTotal = 0.0f;
+
+	private Vector3 originalPos;
+
+	void Start()
+	{
+		originalPos = transform.position;
+	}
+
+	void Update()
+	{
+		transform.position += new Vector3( 0.0f, scrollSpeed, 0.0f );
+		moveTotal += scrollSpeed;
+		if( moveTotal >= 15.0f )
+		{
+			moveTotal = 0.0f;
+			transform.position = originalPos;
+		}
+	}
+}
