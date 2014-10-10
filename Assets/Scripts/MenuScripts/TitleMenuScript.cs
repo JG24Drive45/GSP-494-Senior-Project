c2536dc74689cd721b6b84c4d1543306a8e48c53
Jill <jg24drive45@gmail.com>
2014-10-10 18:21:28 -0400

    Title Art

---
+++ Assets/Scripts/MenuScripts/TitleMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMenuScript : MonoBehaviour 
+{
+	void Update () 
+	{
+		if( Input.anyKeyDown )
+		{
+			Application.LoadLevel( "MainMenu" );
+		}
+	}
+}
